0a2b76fa7c9647bf2ed9f4a004f2d0b0b2a3beff
Anthea2008 <anthea.randriamihaja@gmail.com>
2026-07-26 23:11:57 +0300

    Update js.javaScript.js

--- js.javaScript.js
+++ js.javaScript.js
@@ -16,4 +16,17 @@ function calculateScore() {
     document.getElementById("result").innerText =
         "Votre score est : " + result + " / " + totalQuestions;
 }
+    // Fonction pour envoyer un événement personnalisé à Google Analytics (GA4)
+function trackStudentAction(actionName) {
+  if (typeof gtag === 'function') {
+    gtag('event', 'student_interaction', {
+      'event_category': 'Espace Etudiant',
+      'event_label': actionName
+    });
+    console.log("Événement envoyé à GA4 : " + actionName);
+  }
+}
+
+// Exemple d'utilisation sur un bouton de téléchargement :
+// <button onclick="trackStudentAction('Téléchargement Planning')">Télécharger le Planning</button>
 </script>
